b030c2fbdf650aa98f15d11770763cdc44dc285a
Jordy4D_LinuxVM <jsadams27@gmail.com>
2024-02-26 22:22:12 -0500

    add brief psuedocode outline in js file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-console.log('hello');
+// psuedocode
+//
+// getUserChoice =  prompt 3 choices for a user to select: rock, paper, or scissors (none case-sensitive answers
+//                  should work here)
+// getComputerChoice = bot generates a random choice between rock, paper, or scissors
+//                      (RNG and assign values 0-33, 34-66, 67-100)
+// rock beats scissors
+// paper beats rock
+// scissors beats paper
+// ^^^assign numerical value for easier comparison?
+//
+// playerSelection = getUserChoice
+// computerSelection = getComputerChoice
+//
+// code compares both answers to make a decision on who wins
+//  if user and bot guesses are equal, guess again
+//  if user guesses rock and bot guesses scissors, user wins
+//  if user guesses paper and bot guesses rock, user wins
+//  if user guesses scissors and bot guesses paper, user wins
+//  if user guesses rock and bot guesses paper, bot wins
+//  if user guesses paper and bot guesses scissors, bot wins
+//  if user guesses scissors and bot guesses rock, bot wins
+
