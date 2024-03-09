fc532354e7362c45a7e188f33953e0706f2b7ea9
JordanAdams_MacBook <jsadams27@gmail.com>
2024-03-09 10:25:16 -0500

    completes Rock Paper Scissors revisted, game now functions with a crudely built web UI

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Generates random computer choice
 function getComputerChoice() {
     let rng = Math.floor((Math.random()*100) + 1);
     if (rng < 34 && rng > 0) {
@@ -10,29 +9,115 @@ function getComputerChoice() {
     }
 };
 
-let computerChoice = getComputerChoice(); // sets computer choice function to a variable for easy calling
-let userChoice;
+function score() {
+    result = winner;
 
-const startGame = document.querySelectorAll('button');
-startGame.forEach((button) => {
+    if ( winner.includes('win') == true) {
+        return userScore += 1;
+    } else if ( winner.includes('lose') == true ) {
+        return computerScore += 1;
+    } else {
+        return tieGame += 1;
+    }
+}
+
+
+
+const choices = document.querySelectorAll('.choices');
+// const button = document.querySelector('');
+
+choices.forEach(button => {
     button.addEventListener('click', () => {
-        playGame();
-    });
-});
+        userChoice = button.innerHTML.toLowerCase();
+        let computerRoundChoice = getComputerChoice();
+        
+        playRound(button.innerHTML.toLowerCase(), computerRoundChoice);
+        score();
 
-// function testAlert() {
-//     alert('yay!');
-// }
+        userRes.textContent = 'You chose: ' + `${button.innerHTML}`;
+        computerRes.textContent = 'The Computer chose: ' + `${computerRoundChoice.toUpperCase()}`;
+        userScoreboard.textContent = 'Your Score: ' + `${userScore}`; //add text to resContainer
+        compScoreboard.textContent = 'Computer Score: ' + `${computerScore}`;
+        tieScoreboard.textContent = "Ties: " + `${tieGame}`;
+        winnerScoreboard.textContent = 'Result: '+ `${winner}`
+        
+        if (userScore === 5) {
+            winnerRes.textContent = "You win!";
+            stopGame();
+        } else if (computerScore === 5) {
+            winnerRes.textContent = "Computer wins!"
+            stopGame();
 
-const rock = document.getElementById('rock');
+        }  
 
-rock.addEventListener('click', () {
-});
+    })
+})
 
+//disables buttons to stop the game after a winner is declared
+function stopGame() {
+    choices.forEach(button => {
+        button.disabled = true
+    })
+}
+
+const results = document.querySelector('.results'); // .results div in html doc
+
+//creates the container for the scoreboard
+const resContainer = document.createElement('div'); //creating new div 'resContainer'
+resContainer.classList.add('resContainer'); // add class name resContainer
+
+//display user's choice to scoreboard
+const userRes = document.createElement('div'); //creating new div 'resContainer'
+userRes.classList.add('userRes'); // add class name resContainer
+
+//display computer's choice to scoreboard
+const computerRes = document.createElement('div'); //creating new div 'resContainer'
+computerRes.classList.add('computerRes'); // add class name resContainer
+
+//display who won
+const winnerScoreboard = document.createElement('div'); //creating new div 'resContainer'
+winnerScoreboard.classList.add('winnerScoreboard'); // add class name resContainer
+
+//display user's score
+const userScoreboard = document.createElement('div'); //creating new div 'resContainer'
+userScoreboard.classList.add('userScoreboard'); // add class name resContainer
+
+//display ties
+const tieScoreboard = document.createElement('div'); //creating new div 'resContainer'
+tieScoreboard.classList.add('tieScoreboard'); // add class name resContainer
+
+//display computer's score
+const compScoreboard = document.createElement('div'); //creating new div 'resContainer'
+compScoreboard.classList.add('compScoreboard'); // add class name resContainer
+
+//display winner announcement
+const winnerRes = document.createElement('div'); //creating new div 'resContainer'
+winnerRes.classList.add('winnerRes'); // add class name resContainer
+
+userRes.textContent = 'You Chose: ';
+computerRes.textContent = 'The Computer chose: ';
+winnerScoreboard.textContent = 'Result: ';
+userScoreboard.textContent = 'Your Score:'; //add text to resContainer
+compScoreboard.textContent = 'Computer Score: ';
+tieScoreboard.textContent = 'Ties: ';
+
+results.appendChild(userRes);
+results.appendChild(computerRes);
+results.appendChild(winnerScoreboard);
+results.appendChild(userScoreboard);
+results.appendChild(compScoreboard);
+results.appendChild(tieScoreboard);
+results.appendChild(winnerRes);
+
+
+// Generates random computer choice
+
+
+let computerChoice = getComputerChoice(); // sets computer choice function to a variable for easy calling
+let winner;
 
 // comparison logic for Rock, Paper, Scissors
 function playRound(userChoice, computerChoice) {
-    let winner;
     if (userChoice == 'rock' && computerChoice == 'paper') {
         winner = 'You lose! Paper beats rock.';
     } else if (userChoice == 'paper' && computerChoice == 'scissors') {
@@ -55,43 +140,4 @@ function playRound(userChoice, computerChoice) {
 let userScore = 0;
 let computerScore = 0;
 let tieGame = 0;
-
-// game function
-function playGame() {
-    
-    
-    getComputerChoice();
-    let compChoiceLoop = getComputerChoice();
-    // let userGameChoice = prompt('Choose your weapon: Rock, Paper, or Scissors. ').toLowerCase(); //gets user's input for game
-    let userGameChoice = userChoice;    
-
-    console.log('The Computer Chose: ' + compChoiceLoop);
-    console.log('You Chose: ' + userGameChoice);
-    console.log(playRound(userGameChoice, compChoiceLoop));
-    
-
-
-    function score() {
-    let result = playRound(userGameChoice, compChoiceLoop);
-
-
-    if ( result.includes('win') == true) {
-            return userGameChoice += 1;
-        } else if ( result.includes('lose') == true ) {
-            return computerScore += 1;
-        } else {
-            return tieGame += 1;
-        }
-    }
-    score();
-    console.log('Your Score: ' + userScore);
-    console.log("Computer's Score: " + computerScore);
-    console.log("There have been " + tieGame + " ties!");
-}
-
-// loops the game 5 times
-// for (let i = 0; i < 5; i++) {
-//     playGame();
-// }
-
-
+let result;
